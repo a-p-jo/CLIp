e02a3b4ab5d76c5fc5452908451be5b9a9a3d24d
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2021-02-25 17:56:40 +0530

    CLIp v2
    New : Multiple clipboards in one command
    Removed : Suffix for every clipboard

--- clip.c
+++ clip.c
@@ -3,150 +3,135 @@
 #include <stdint.h>// uint_fast8_t uint_fast64_t
 #include <string.h>// strcpy() strcat() strcmp()
 
-#define SUFFIX ".clipboard.clip" // suffix + clipboard name (optional) = filename of clipboard's file.
+#define DEFAULT ".clipboard.clip"
+#define TMP_FILE_NAME ".temporary.clip"
 
-char * combine(char *, char *); // combine's two strings one-after-the-other into a sufficiently sized new string, which it returns 
-int bcp(FILE *, FILE*);	// bcp == bytecopy, copies one stream to the other (here stdin/stdout and clipboard file) byte-accurately through a buffer
+#define CLEAN_TMP fclose(tmp); remove(TMP_FILE_NAME);
+
+#define BLOCK (1*1048576) // Size of buffer for bcp() in bytes . 1 MiB by default.
+int bcp(FILE *, FILE*, char *, char *);	// bcp == bytecopy, copies one stream to the other (here stdin/stdout and clipboard file) byte-accurately through a buffer
 
 int main(int argc, char * argv[])
 {
-	 /* Usage Examples :
-	  * dmesg | clip c                 # Save output of dmesg to default clipboard in current working directory (cwd)
-	  * lsblk | clip c drives          # Save output of lsblks to a clipboard named drives in cwd 
-	  * 
-	  * clip p | gzip > dmesg.zip      # Pipe default clipboard in cwd to gzip , which saves it to a .zip file
-	  * clip p drives >> drive_log.txt # Pipe clipboard named drives in cwd to drive_log.txt 
-	  *
-	  * clip r			   # Remove the default clipboard in the cwd
-	  * clip r drives		   # Remove the clipboard named drives in the cwd
-	  */
-	if(argc >= 2) 
+	if(argc >= 2) // Usage : clip [operation] [clipboard name (optional)] [clipboard name (optional)]...
 	{
-		uint_fast8_t clip_named = (argc >= 3);
-
-		if(!strcmp(argv[1],"paste") || !strcmp(argv[1],"p"))
+		// Process arguments
+		if(argc >= 3) // If named clipboard(s) are given, convert any empty names to DEFAULT
 		{
-			char * clip_name = ""; // initialise string to empty, null-terminated.
+			for(uint_fast8_t i = 2; argv[i] != NULL; i++)
+			{
+				if(strlen(argv[i]) == 0)
+					argv[i] = DEFAULT;
+			}
+		}		
+		else // argv[argc] is defined to exist (NULL). If no names are given, change it to point to DEFAULT, and suitably increment argc
+		{
+			argv[argc] = DEFAULT;
+			argc++;
+		}
+		//Now we can iterate over argv and use the strings directly in the rest of the code, without rewriting the above for each scenario.
 
-			if(clip_named)
-				clip_name = combine(argv[2],SUFFIX); // if clipboard is named , add the suffix to it
-			else
-				clip_name = SUFFIX; // if clipboard is not named, it's name IS suffix.
-
-			if(clip_name != NULL)
+		if(!strcmp(argv[1],"p") || !strcmp(argv[1],"paste"))
+		{
+			for(uint_fast8_t i = 2; i < argc; i++) // Loop over argv , and write every clipboard to stdout
 			{	
-				FILE * from = fopen(clip_name,"rb");
+				FILE * from = fopen(argv[i],"rb");
 
 				if(from == NULL)
 				{
-					perror("Failed : Error opening clipboard ");
+					fprintf(stderr,"Failed : Error opening clipboard \" %s \" : ",argv[i]);
+					perror("");
 					return 1;
 				}
 
-				int status = bcp(from,stdout); // try to copy all bytes from clipboard to stdout
+				int status = bcp(from,stdout, argv[i],"stdout"); // try to copy all bytes from clipboard to stdout
 
-				if(clip_named)
-					free(clip_name); // combine() returns a malloc'd char * , which we have to free 
+				fclose(from);
 
-				fclose(from); 
-				return status;
+				if(status) 
+					return status; //exit at first failed failed paste
 			}
-			else
-				goto malloc_error; // if combine() returns NULL , malloc() in it had returned NULL , i.e. , failed.
-
 		}
 
-		else if (!strcmp(argv[1],"copy") || !strcmp(argv[1],"c"))
+		// copying and appending is essentially the same thing, the difference only being an "ab" instead of "wb" to fopen.
+		else if (!strcmp(argv[1],"c") || !strcmp(argv[1],"copy") || !strcmp(argv[1], "a") || !strcmp(argv[1], "append"))
 		{
-			char * clip_name = "";
+			FILE * tmp = fopen(TMP_FILE_NAME,"w+"); // Create a temp stream which can be written to and also read 
 
-			if(clip_named)
-				clip_name = combine(argv[2],SUFFIX);
-			else
-				clip_name = SUFFIX;
+			if(tmp == NULL) 
+			{
+				perror("Failed : Error opening temporary file ");
+				return 8;
+			}
 
-			if(clip_name != NULL)
+			int rval = bcp(stdin,tmp,"stdin","temporary file"); // Copy stdin to the temp stream, as stdin is not rewindable
+
+			if(rval)
+				return rval;
+
+			fflush(tmp); // Flush temp stream to ensure all data is written to it by now
+			rewind(tmp); // Rewind it so it can be read from the begining in the loop
+
+			for(uint_fast8_t i = 2; i < argc; i++) // Write data from temp stream to every clipboard
 			{	
-				FILE * to = fopen(clip_name,"wb");
+				FILE * to = fopen(argv[i],(*argv[1] == 'c') ? "wb" : "ab"); // Open to write or append as asked to
 
 				if(to == NULL)
 				{
-					perror("Failed : Error opening clipboard ");
+					fprintf(stderr,"Failed : Error opening clipboard \" %s \" : ",argv[i]);
+					perror("");
+					CLEAN_TMP
 					return 3;
 				}
 
-				int status = bcp(stdin,to);
-				if(clip_named)
-					free(clip_name);
+				int status = bcp(tmp,to,"temporary file",argv[i]); // Copy from temp stream to clipboard
+				rewind(tmp); // Rewind temp stream, so it can be re-read in next iteration
 
-				if(fclose(to) != 0)
+				if(fclose(to)) // Close this clipboard
 				{
-					perror("Failed : Error saving to clipboard ");
+					fprintf(stderr,"Failed : Error saving to clipboard \" %s \" : ",argv[i]);
+					perror("");
+					CLEAN_TMP
 					return 4;
 				}
 
-				return status;
+				if(status)
+					return status;
 			}
-			else
-			{
-				malloc_error :
-				fprintf(stderr,"Failed : Error allocating memory for clipboard filename\n"); // Only print to stderr , as stdout must not be tampered with
-				return 6;
-			}
+			CLEAN_TMP // Cleanup , i.e., close the temp stream and delete the file from disk.
 		}
 
-		else if (!strcmp(argv[1],"remove") || !strcmp(argv[1],"r"))
+		else if (!strcmp(argv[1],"remove") || !strcmp(argv[1],"r")) 
 		{
-			char * torm = "";
-
-			if(clip_named)
-				torm = combine(argv[2],SUFFIX);
-			else
-				torm = SUFFIX;
-
-			int returnval = 0;
-
-			if(remove(torm)) // remove() returns non-zero on failure 
+			int rval = 0; 
+			for(uint_fast8_t i = 2; i < argc; i++)	// Remove every clipboard given		
 			{
-				perror("Failed ");
-				returnval = 7; // do not immediately return , as we may need to free clip_name 
+				if(remove(argv[i])) // remove() returns non-zero on failure 
+				{
+					fprintf(stderr,"Failed : Error removing clipboard \" %s \" : ",argv[i]);
+					perror("");
+					rval = 7; // do not immediately return , as we may have other files to set.
+				}
 			}
-			
-			if(clip_named)
-				free(torm);
-
-			return returnval;
+			return rval;
 		}
 		else
 		{
 			fprintf(stderr,"Failed : Unrecognizable arguments.\n");
-			return 8;
+			return 9;
 		}
+
+		return 0; // If we've reached here, we were successful
 	}
 
 	else
 	{
-		fprintf(stderr,"Failed : Not enough arguments");
-		return 9;
+		fprintf(stderr,"Failed : Not enough arguments\n");
+		return 10;
 	}
 }
 
-char * combine(char * original, char * toadd) // combine contents of two strings into a new one exactly big enough to accomodate both + \0 and return it
-{
-	char * result = malloc( strlen(original) + strlen(toadd) + sizeof('\0') );
-	// Must free later
-
-	if(result == NULL) return result;
-
-	strcpy(result,original); // copy 1st str to new string
-	strcat(result,toadd);    // append 2nd string to new string
-
-	return result;           // return new string
-}
-
-#define BLOCK (1*1048576) // Size of buffer for bcp() in bytes . 1 MiB by default.
-
-int bcp(FILE * from, FILE * to)
+int bcp(FILE * from, FILE * to, char * fromname, char * toname)
 {
         uint_fast8_t buffer[BLOCK]; // Buffer . Static - on stack or possibly cache , potentially faster than malloc(). Need only have one-byte elements, but whatever's fastest.
         uint_fast64_t bytes_processed = 0;
@@ -158,13 +143,13 @@ int bcp(FILE * from, FILE * to)
         if(ferror(from) || ferror(to)) // If bytes read were less than BLOCK size, check for errors 
 	{
             if(ferror(from))
-                fprintf(stderr,"Failed : Unknown fatal error reading\n");
+                fprintf(stderr,"Failed : Unknown fatal error reading from \" %s \"\n",fromname);
             if(ferror(to))
                 failed_fwrite : 
-                fprintf(stderr,"Failed : Unknown fatal error writing\n");
+                fprintf(stderr,"Failed : Unknown fatal error writing to \" %s \"\n",toname);
             
             fprintf(stderr,"Forced to abandon at %llu bytes... exiting...\n",(long long unsigned)(bytes_processed));
-            return 10;
+            return 11;
         }
 
         else // If no errors, we've reached EOF. If there's any leftover bytes (likely) , write them
