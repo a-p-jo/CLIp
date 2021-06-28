3219fcb3dad510e24e56b83f04eedce27d3bf5cc
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2021-06-28 10:48:21 +0530

    v2.1
    1. Updated and changed comment wording and style
    2. Reworked error handling : changed and organised error codes, changes exiting from main from returns to exit()s, Used errno and strerror instead of perror() in most places, added error checking for fflush and rewinding streams.
    3. main() no longer parses commands with strcmp() and if-else . Changed to checking only first char of arg with switch-case. This now allows upper case args, and faster parsing.
    4. For some reason, argv was iterated with uint_fast8_t , changed to int.

--- clip.c
+++ clip.c
@@ -1,146 +1,35 @@
-#include <stdio.h> // fread() fwrite() fprintf() perror() 
-#include <stdlib.h>// malloc() free()
-#include <stdint.h>// uint_fast8_t uint_fast64_t
-#include <string.h>// strcpy() strcat() strcmp()
+#include <stdio.h>  /* fread() fwrite() fprintf() strerror() perror() fopen() fclose() ferror() feof() FILE remove() */
+#include <stdlib.h> /* exit() 											     */
+#include <stdint.h> /* uint_fast8_t uint64_t									     */
+#include <string.h> /* strcmp() 										     */
+#include <errno.h>  /* errno											     */
 
-#define DEFAULT ".clipboard.clip"
-#define TMP_FILE_NAME ".temporary.clip"
+/* Constants */
+#define DEFAULT   ".clipboard.clip"
+#define TMP_FNAME ".temporary.clip"
 
-#define CLEAN_TMP fclose(tmp); remove(TMP_FILE_NAME);
-
-#define BLOCK (1*1048576) // Size of buffer for bcp() in bytes . 1 MiB by default.
-int bcp(FILE *, FILE*, char *, char *);	// bcp == bytecopy, copies one stream to the other (here stdin/stdout and clipboard file) byte-accurately through a buffer
-
-int main(int argc, char * argv[])
-{
-	if(argc >= 2) // Usage : clip [operation] [clipboard name (optional)] [clipboard name (optional)]...
-	{
-		// Process arguments
-		if(argc >= 3) // If named clipboard(s) are given, convert any empty names to DEFAULT
-		{
-			for(uint_fast8_t i = 2; argv[i] != NULL; i++)
-			{
-				if(strlen(argv[i]) == 0)
-					argv[i] = DEFAULT;
-			}
-		}		
-		else // argv[argc] is defined to exist (NULL). If no names are given, change it to point to DEFAULT, and suitably increment argc
-		{
-			argv[argc] = DEFAULT;
-			argc++;
-		}
-		//Now we can iterate over argv and use the strings directly in the rest of the code, without rewriting the above for each scenario.
-
-		if(!strcmp(argv[1],"p") || !strcmp(argv[1],"paste"))
-		{
-			for(uint_fast8_t i = 2; i < argc; i++) // Loop over argv , and write every clipboard to stdout
-			{	
-				FILE * from = fopen(argv[i],"rb");
-
-				if(from == NULL)
-				{
-					fprintf(stderr,"Failed : Error opening clipboard \" %s \" : ",argv[i]);
-					perror("");
-					return 1;
-				}
-
-				int status = bcp(from,stdout, argv[i],"stdout"); // try to copy all bytes from clipboard to stdout
-
-				fclose(from);
-
-				if(status) 
-					return status; //exit at first failed failed paste
-			}
-		}
-
-		// copying and appending is essentially the same thing, the difference only being an "ab" instead of "wb" to fopen.
-		else if (!strcmp(argv[1],"c") || !strcmp(argv[1],"copy") || !strcmp(argv[1], "a") || !strcmp(argv[1], "append"))
-		{
-			FILE * tmp = fopen(TMP_FILE_NAME,"w+"); // Create a temp stream which can be written to and also read 
-
-			if(tmp == NULL) 
-			{
-				perror("Failed : Error opening temporary file ");
-				return 8;
-			}
-
-			int rval = bcp(stdin,tmp,"stdin","temporary file"); // Copy stdin to the temp stream, as stdin is not rewindable
-
-			if(rval)
-				return rval;
-
-			fflush(tmp); // Flush temp stream to ensure all data is written to it by now
-			rewind(tmp); // Rewind it so it can be read from the begining in the loop
-
-			for(uint_fast8_t i = 2; i < argc; i++) // Write data from temp stream to every clipboard
-			{	
-				FILE * to = fopen(argv[i],(*argv[1] == 'c') ? "wb" : "ab"); // Open to write or append as asked to
-
-				if(to == NULL)
-				{
-					fprintf(stderr,"Failed : Error opening clipboard \" %s \" : ",argv[i]);
-					perror("");
-					CLEAN_TMP
-					return 3;
-				}
-
-				int status = bcp(tmp,to,"temporary file",argv[i]); // Copy from temp stream to clipboard
-				rewind(tmp); // Rewind temp stream, so it can be re-read in next iteration
-
-				if(fclose(to)) // Close this clipboard
-				{
-					fprintf(stderr,"Failed : Error saving to clipboard \" %s \" : ",argv[i]);
-					perror("");
-					CLEAN_TMP
-					return 4;
-				}
-
-				if(status)
-					return status;
-			}
-			CLEAN_TMP // Cleanup , i.e., close the temp stream and delete the file from disk.
-		}
-
-		else if (!strcmp(argv[1],"remove") || !strcmp(argv[1],"r")) 
-		{
-			int rval = 0; 
-			for(uint_fast8_t i = 2; i < argc; i++)	// Remove every clipboard given		
-			{
-				if(remove(argv[i])) // remove() returns non-zero on failure 
-				{
-					fprintf(stderr,"Failed : Error removing clipboard \" %s \" : ",argv[i]);
-					perror("");
-					rval = 7; // do not immediately return , as we may have other files to set.
-				}
-			}
-			return rval;
-		}
-		else
-		{
-			fprintf(stderr,"Failed : Unrecognizable arguments.\n");
-			return 9;
-		}
-
-		return 0; // If we've reached here, we were successful
-	}
-
-	else
-	{
-		fprintf(stderr,"Failed : Not enough arguments\n");
-		return 10;
-	}
-}
+/* Exit Codes */
+#define SUCCESS 	    0
+#define CLIPBOARD_OPEN_ERR  1
+#define CLIPBOARD_CLOSE_ERR 2
+#define TMP_OPEN_ERR        3
+#define TMP_FFLUSH_ERR      4
+#define TMP_REWIND_ERR      5
+#define UNRECOGNISED_ARGS   6
+#define NOT_ENOUGH_ARGS     7
+#define BCP_MID_IO_ERR      8
 
 int bcp(FILE * from, FILE * to, char * fromname, char * toname)
 {
-        uint_fast8_t buffer[BLOCK]; // Buffer . Static - on stack or possibly cache , potentially faster than malloc(). Need only have one-byte elements, but whatever's fastest.
-        uint_fast64_t bytes_processed = 0;
-        uint_fast64_t bytes_read; // Stores number of bytes read in each iteration , so write *only* as much as read when less than BLOCK bytes read.
+        #define BLOCK (1048576) 
+	uint_fast8_t buffer[BLOCK];    /* Static buffer; faster than malloc(). Need only one-byte per element, but whatever's fastest. */
+        uint64_t bytes_processed = 0;
+        uint64_t bytes_read;
 
-        while((bytes_read = fread(buffer,1,BLOCK,from)) == BLOCK && fwrite(buffer,1,BLOCK,to) == BLOCK) // "&&" Means fwrite() won't execute if fread() condition fails, so no garbage
+        while((bytes_read = fread(buffer,1,BLOCK,from)) == BLOCK && fwrite(buffer,1,BLOCK,to) == BLOCK) /* fwrite() won't execute if fread() fails */
 		bytes_processed += BLOCK;
 
-        if(ferror(from) || ferror(to)) // If bytes read were less than BLOCK size, check for errors 
+        if(ferror(from) || ferror(to)) /* If bytes read were less than BLOCK size, check for errors */
 	{
             if(ferror(from))
                 fprintf(stderr,"Failed : Unknown fatal error reading from \" %s \"\n",fromname);
@@ -149,13 +38,131 @@ int bcp(FILE * from, FILE * to, char * fromname, char * toname)
                 fprintf(stderr,"Failed : Unknown fatal error writing to \" %s \"\n",toname);
             
             fprintf(stderr,"Forced to abandon at %llu bytes... exiting...\n",(long long unsigned)(bytes_processed));
-            return 11;
+            return BCP_MID_IO_ERR;
         }
-
-        else // If no errors, we've reached EOF. If there's any leftover bytes (likely) , write them
+        else 			       /* If no errors, we've reached EOF. If there's any leftover bytes (likely) , write them */
         {
-            if(bytes_read && fwrite(buffer,1,bytes_read,to) != bytes_read) // "&&" Means fwrite() won't wastefully execute if bytes_read == 0
-                goto failed_fwrite; // In the unlikely case the last fwrite() failed, handle it the same way as above
+            if(bytes_read && fwrite(buffer,1,bytes_read,to) != bytes_read)
+                goto failed_fwrite;    /* In the unlikely case the last fwrite() failed, handle it the same way as previously */
         }
-	return 0; // If we got this far without returning, program was successful. Return 0.
+	return SUCCESS;
+}
+
+int main(int argc, char ** argv)
+{
+	/* Usage : clip [operation] [clipboard name (optional)] [clipboard name (optional)]... */
+	if(argc >= 2)
+	{
+		              /* Process arguments */
+		if(argc >= 3) /* If named clipboard(s) are given, convert any empty names to DEFAULT */
+		{
+			for(int i = 2; argv[i]; i++)
+			{
+				if(strlen(argv[i]) == 0)
+					argv[i] = DEFAULT;
+			}
+		}		
+		else          /* argv[argc] is defined to be NULL. If no names are given, change it to point to DEFAULT, and suitably increment argc */
+		{
+			argv[argc] = DEFAULT;
+			argc++;
+		}
+
+		switch(*argv[1])
+		{
+			case 'p': case 'P':           /* Paste */
+			for(int i = 2; i < argc; i++) /* Loop over argv , and write every clipboard to stdout */
+			{	
+				FILE * from = fopen(argv[i],"rb");
+
+				if(!from)
+				{
+					fprintf(stderr,"Failed : Error opening clipboard \" %s \" : %s\n",strcmp(argv[i],DEFAULT)? argv[i] : "default",errno? strerror(errno) : "Unknown error");
+					exit(CLIPBOARD_OPEN_ERR);
+				}
+
+				int status = bcp(from,stdout, argv[i],"stdout");
+				if(status) exit(status);
+				break;
+			}
+			case 'c' : case 'C' : case 'a' : case 'A' : 		    /* Copy or append */
+			{
+				FILE * tmp = fopen(TMP_FNAME,"wb+"); 
+
+				if(!tmp) 
+				{
+					perror("Failed : Error opening temporary file ");
+					exit(TMP_OPEN_ERR);
+				}
+
+				int rval = bcp(stdin,tmp,"stdin","temporary file"); /* Copy stdin to the temp stream, as stdin is not seekable */
+				if(rval) exit(rval);
+
+				if(fflush(tmp))					    /* Flush temp stream to ensure all data is written to it by now */
+				{
+					perror("Failed : Error writing to temporary file ");
+					exit(TMP_FFLUSH_ERR);
+				}
+				if(fseek(tmp,0,SEEK_SET)) 			    /* Rewind it so it can be read from the begining in the loop */
+				{
+					perror("Failed : Error rewinding temporary file ");
+					exit(TMP_REWIND_ERR);
+				}
+
+				for(int i = 2; i < argc; i++) 			    /* Write data from temp stream to every clipboard */
+				{	
+					FILE * to = fopen(argv[i],(*argv[1] == 'c' || *argv[1] == 'C') ? "wb" : "ab");
+
+					if(to == NULL)
+					{
+						fprintf(stderr,"Failed : Error opening clipboard \" %s \" : %s\n",strcmp(argv[i],DEFAULT)? argv[i] : "default",errno? strerror(errno) : "Unknown error");
+						fclose(tmp) ;remove(TMP_FNAME);
+						exit(CLIPBOARD_OPEN_ERR);
+					}
+
+					int status = bcp(tmp,to,"temporary file",argv[i]); 
+					if(fseek(tmp,0,SEEK_SET)) 		    /* Rewind temp stream, so it can be re-read in next iteration */
+					{
+						perror("Failed : Error rewinding temporary file ");
+						exit(TMP_REWIND_ERR);
+					}
+
+					if(fclose(to)) 				    /* Close this clipboard */
+					{
+						fprintf(stderr,"Failed : Error saving to clipboard \" %s \" : %s\n",strcmp(argv[i],DEFAULT)? argv[i] : "default",errno? strerror(errno) : "Unknown error");
+						remove(TMP_FNAME);
+						exit(CLIPBOARD_CLOSE_ERR);
+					}
+
+					if(status) { remove(TMP_FNAME); exit(status); }
+				}
+				fclose(tmp); remove(TMP_FNAME);
+				break;
+			}
+			case 'r' : case 'R': /* Remove */
+			{
+				int rval = 0; 
+				for(int i = 2; i < argc; i++)		
+				{
+					if(remove(argv[i]))
+					{
+						fprintf(stderr,"Failed : Error removing clipboard \" %s \" : %s\n",strcmp(argv[i],DEFAULT)? argv[i] : "default",errno? strerror(errno) : "Unknown error");
+						rval = 7; /* do not immediately exit , as we may have other files to remove */
+					}
+				}
+				exit(rval);
+			}
+			default:
+				fprintf(stderr,"Failed : Unrecognizable arguments.\n");
+				exit(UNRECOGNISED_ARGS);
+				break;
+		}
+
+		exit(SUCCESS);
+	}
+	else
+	{
+		fprintf(stderr,"Failed : Not enough arguments\n");
+		exit(NOT_ENOUGH_ARGS);
+	}
 }
